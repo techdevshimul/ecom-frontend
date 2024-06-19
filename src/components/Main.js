a92058b1505cd564a6a828a4891c667a9cc56010
Shimul Hossain <shimultechtips@gmail.com>
2024-06-19 11:15:32 +0600

    08-E-08

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,9 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./home/Home";
 import Login from "./user/Login";
 import Register from "./user/Register";
+import PrivateRoute from "./protectedRoutes/PrivateRoute";
+import Dashboard from "./user/Dashboard";
 
 const Main = () => {
   return (
@@ -10,6 +12,14 @@ const Main = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </div>
   );
