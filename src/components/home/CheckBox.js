11c2d7d7b3d21bb11149d6502b9a1bf43731ef5b
Shimul Hossain <shimultechtips@gmail.com>
2024-06-21 14:50:40 +0600

    08-F-07

--- src/components/home/CheckBox.js
+++ src/components/home/CheckBox.js
@@ -1,9 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const CheckBox = ({ categories }) => {
+  const [checked, setChecked] = useState([]);
+  const checkedIds = [...checked];
+
+  const handleToggle = (id) => () => {
+    const foundId = checked.indexOf(id);
+    if (foundId === -1) {
+      checkedIds.push(id);
+    } else {
+      checkedIds.splice(foundId, 1);
+    }
+
+    setChecked(checkedIds);
+  };
+
+  useEffect(() => {
+    console.log(JSON.stringify(checked));
+  }, [checked]);
+
   return categories.map((category) => (
     <li className="list-unstyled" key={category._id}>
-      <input type="checkbox" className="form-check-input" />
+      <input
+        type="checkbox"
+        className="form-check-input"
+        onChange={handleToggle(category._id)}
+        value={checked.indexOf(category._id === -1)}
+      />
       <label className="form-check-label ps-2">{category.name}</label>
     </li>
   ));
