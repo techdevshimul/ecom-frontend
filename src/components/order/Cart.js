58c2a0dbfcae3a9fcf5ea8513eb725c853b75116
Shimul Hossain <shimultechtips@gmail.com>
2024-06-23 18:34:31 +0600

    08-G-02

---
+++ src/components/order/Cart.js
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+import Layout from "../Layout";
+import { Link } from "react-router-dom";
+import { getCartItems } from "../../api/apiOrder";
+import { userInfo } from "../../utils/auth";
+import CartItem from "./CartItem";
+
+const Cart = () => {
+  const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    getCartItems(userInfo().token)
+      .then((response) => setCartItems(response.data))
+      .catch((err) => {});
+  }, []);
+
+  return (
+    <Layout
+      title="Your Cart"
+      description="Hurry up! Place your order!"
+      className="container"
+    >
+      <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">
+            <a href="#">Order</a>
+          </li>
+          <li class="breadcrumb-item active" aria-current="page">
+            Cart
+          </li>
+        </ol>
+      </nav>
+      <div className="container my-5">
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col" width="15%">
+                #
+              </th>
+              <th scope="col">Image</th>
+              <th scope="col">Product Name</th>
+              <th scope="col">Quantity</th>
+              <th scope="col" align="right">
+                Price
+              </th>
+              <th scop="col">Remove</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cartItems &&
+              cartItems.map((item, i) => (
+                <CartItem cartItem={item} serial={i + 1} key={item._id} />
+              ))}
+            <tr>
+              <th scope="row" />
+              <td colSpan={2}>Total</td>
+              <td align="right">৳ </td>
+              <td />
+            </tr>
+            <tr>
+              <th scope="row" />
+              <td colSpan={4} className="text-right">
+                <Link to="/">
+                  <button className="btn btn-warning mr-4">
+                    Continue Shoping
+                  </button>
+                </Link>
+                <Link to="/checkout" className="btn btn-success mr-4">
+                  Proceed To Checkout
+                </Link>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </Layout>
+  );
+};
+
+export default Cart;
